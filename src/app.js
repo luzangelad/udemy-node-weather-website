7cec6bf54ec87fd96b9397874906d4365d21ae6c
management evolution <test@debian.luz.com>
2019-04-12 16:04:54 -0400

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define path for express config
 const publicDir = path.join(__dirname, "../public")
@@ -83,6 +84,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('on port 3000')
+app.listen(port, () => {
+    console.log('on port ' + port)
 })
